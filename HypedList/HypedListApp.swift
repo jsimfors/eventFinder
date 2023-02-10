b66be839c5a8ebf69d2b36921f7f748f6a8aed50
jsimfors <johanna.simfors@gmail.com>
2023-02-10 16:38:20 +0100

    Initial Commit

---
+++ HypedList/HypedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypedListApp.swift
+//  HypedList
+//
+//  Created by Johanna Simfors on 2023-02-10.
+//
+
+import SwiftUI
+
+@main
+struct HypedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
